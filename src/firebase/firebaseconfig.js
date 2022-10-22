810dec7154211ed6e68ffd65e80da6ea1858a2a6
Edmilson Silva <scovinowork@gmail.com>
2022-10-22 13:29:54 -0300

    update

--- src/firebase/firebaseconfig.js
+++ src/firebase/firebaseconfig.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAI,
+    authDomain: process.env.AUTHDOMAIN,
     projectId: process.env.PROJECTID,
     storageBucket:process.env.STORAGEBUCKET,
     messagingSenderId: process.env.MENSSAGINGSENDERID,
