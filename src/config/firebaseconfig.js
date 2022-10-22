3840eaf3b3060f6e677a878bf3fb9de836759b78
Edmilson Silva <scovinowork@gmail.com>
2022-10-22 16:17:42 -0300

    update

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from '@firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArE8E4PAiNWr0hi0gERed-pwhVOn6a9BI",
+  authDomain: "rickmortyapp-91fcd.firebaseapp.com",
+  projectId: "rickmortyapp-91fcd",
+  storageBucket: "rickmortyapp-91fcd.appspot.com",
+  messagingSenderId: "316825606696",
+  appId: "1:316825606696:web:0c6d8d9f9ec1969193331d"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
